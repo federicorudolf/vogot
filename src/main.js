be04e93142e0dc063730bccec0dbff2a7470c594
Federico Rudolf <federico.rudolf@globant.com>
2018-08-20 23:27:16 -0300

    Structured app

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
+import 'vuetify/dist/vuetify.min.css'
 import Vue from 'vue'
 import App from './App.vue'
+import Vuetify from 'vuetify'
+import router from './router'
+
+Vue.use(Vuetify)
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
---
+++ src/pages/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <router-view></router-view>
+    <app-header></app-header>
+  </div>
+</template>
+<script>
+import header from './components/Header';
+
+export default {
+  components: {
+    appHeader: header,
+  },
+  data() {
+    return {
+
+    }
+  }
+  
+}
+</script>
